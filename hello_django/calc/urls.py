061ad8510edf487ecc96f3912654e65bf6e8bbfe
Sergey Karpuk <sergeykarpuk@yandex.by>
2022-07-15 15:29:13 +0300

    add last sums list

--- hello_django/calc/urls.py
+++ hello_django/calc/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from hello_django.calc.views import index
+from hello_django.calc.views import index, history
 
 urlpatterns = [
     path('<int:a>/<int:b>', index.as_view(), name='calc'),
+    path('history', history.as_view())
 ]
--- hello_django/calc/views.py
+++ hello_django/calc/views.py
@@ -1,10 +1,22 @@
 from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
+from hello_django.calc.models import History
 
 
 class index(View):
 
     def get(self, request, a, b):
         sum = a + b
+        History(value=sum).save()
         return render(request, 'calc/index.html', {'a': a, 'b': b, 'sum': sum})
+
+
+class history(View):
+
+    def get(self, request):
+        context = {}
+        # context['results'] = History.objects.all()[0:10]
+        context['results'] = History.objects.order_by('-timestamp')[0:10]
+
+        return render(request, 'calc/history.html', context)
